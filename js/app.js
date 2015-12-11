b4cf077bdce70ab03fff2fd2470405efb94ae6dc
phobos101 <robwilson101@gmail.com>
2015-12-11 17:45:41 +0000

    Moved code input into a bootstrap column

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-var myCodeMirror = CodeMirror(document.body);
+var codeInput = document.body.children[0].children[0].children[1];
+var myCodeMirror = CodeMirror(codeInput, {
+  value: "function welcome(){return 'Welcome to Coffee Coder';}\n",
+  mode:  "javascript"
+});
