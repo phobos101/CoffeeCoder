a818a8643251218f7313aab7c0f2d6b1e06214b0
Rob Wilson <robwilson101@gmail.com>
2015-12-12 11:35:32 +0000

    Added writeln function for line breaks on loops etc

--- js/app.js
+++ js/app.js
@@ -9,9 +9,30 @@ var myCodeMirror = CodeMirror(codeInput, {
   mode:  "javascript"
 });
 
-function putOutput() {
+function runCode() {
   event.preventDefault();
   var input = myCodeMirror.getValue();
-  result = eval(input);
-  codeOutput.textContent = result;
+  codeOutput.textContent = "";
+  var result = evalCode(eval(input));
+  if (result != undefined) codeOutput.innerHTML += result;
+};
+
+evalCode = function(input) {
+    var result = "["
+    if (typeof(input)=="object" && input.length) {
+        for (var i=0; i < input.length; i++)
+            if (typeof(input[i])=="object" && input[i].length) {
+                result += (i==0?"":" ")+"["
+                for (var j=0; j<input[i].length; j++)
+                    result += input[i][j]+(j==input[i].length-1?
+                            "]"+(i==input.length-1?"]":",")+"\n":", ");
+            } else result += input[i]+(i==input.length-1?"]":", ");
+    } else result = input;
+    return result;
+};
+
+writeln = function(input) {
+    if (!input) input="";
+    codeOutput.innerHTML += evalCode(input);
+    codeOutput.innerHTML += "<br>";
 };
