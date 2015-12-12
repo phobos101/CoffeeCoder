3ea9497063f9b820dc0f8a5e03cdcec1f3a52287
Rob Wilson <robwilson101@gmail.com>
2015-12-12 10:51:49 +0000

    Added meta tags, output section, output JS, and run button

--- js/app.js
+++ js/app.js
@@ -1,5 +1,17 @@
-var codeInput = document.body.children[0].children[0].children[1];
+var codeInput = document.getElementById('codeInput');
+var codeOutput = document.getElementById('codeOutput');
+
 var myCodeMirror = CodeMirror(codeInput, {
-  value: "function welcome(){return 'Welcome to Coffee Coder';}\n",
+  lineNumbers: true,
+  autoCloseBrackets: true,
+  matchBrackets: true,
+  value: "(function welcome() {\n  return 'CoffeeCoder Rocks!';\n})();\n",
   mode:  "javascript"
 });
+
+function putOutput() {
+  event.preventDefault();
+  var input = myCodeMirror.getValue();
+  result = eval(input);
+  codeOutput.textContent = result;
+};
