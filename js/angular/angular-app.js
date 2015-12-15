60ca2159cb909e401bb260528bfc4d1f08b69b02
phobos101 <robwilson101@gmail.com>
2015-12-15 16:42:53 +0000

    Added authInterceptor to check for token

--- js/angular/angular-app.js
+++ js/angular/angular-app.js
@@ -1,12 +1,14 @@
 angular
-  .module('CoffeeCoder', ['ui.router'])
+  .module('CoffeeCoder', ['ui.router', 'ngResource', 'angular-jwt'])
+  .constant('API', 'http://localhost:3000/')
   .config(MainConfig);
 
-function MainConfig($stateProvider, $urlRouterProvider) {
+function MainConfig($stateProvider, $urlRouterProvider, $httpProvider) {
+  $httpProvider.interceptors.push('authInterceptor');
   $stateProvider
     .state('code', {
       url: '/',
       templateUrl: '_code.html'
     });
-  $urlRouterProvider.otherwise('/');  
+  $urlRouterProvider.otherwise('/');
 };
