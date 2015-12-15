a90d57f746961bdbc89a45e6bf3dfd68f24a2d11
phobos101 <robwilson101@gmail.com>
2015-12-15 18:54:01 +0000

    Added fornt end route

--- js/app.js
+++ js/app.js
@@ -39,6 +39,10 @@ writeln = function(input) {
 };
 
 $(function() {
+  $('.code-nav').on('click', function() {
+    event.preventDefault();
+  });
+  
   $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
     event.preventDefault();
     event.stopPropagation();
