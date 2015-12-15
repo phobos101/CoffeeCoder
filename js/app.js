3c8a48c25cb46d6920dba545a89e2dbb6911d3cd
phobos101 <robwilson101@gmail.com>
2015-12-15 16:35:46 +0000

    Added navbar

--- js/app.js
+++ js/app.js
@@ -37,3 +37,21 @@ writeln = function(input) {
     codeOutput.innerHTML += evalCode(input);
     codeOutput.innerHTML += "<br>";
 };
+
+$(function() {
+  $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+
+    $(this).parent().addClass('open');
+
+    var menu = $(this).parent().find("ul");
+    var menupos = menu.offset();
+    if ((menupos.left + menu.width()) + 30 > $(window).width()) {
+        var newpos = - menu.width();
+    } else {
+        var newpos = $(this).parent().width();
+    };
+    menu.css({left:newpos});
+  });
+});
