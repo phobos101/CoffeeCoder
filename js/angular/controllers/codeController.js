1212b7470c390999bcd09668a38f049b684fca38
phobos101 <robwilson101@gmail.com>
2015-12-17 11:30:59 +0000

    Working feedback on user input

--- js/angular/controllers/codeController.js
+++ js/angular/controllers/codeController.js
@@ -2,8 +2,8 @@ angular
   .module('CoffeeCoder')
   .controller('CodeController', CodeController);
 
-CodeController.$inject = ['$stateParams', '$http'];
-function CodeController($stateParams, $http) {
+CodeController.$inject = ['$stateParams', '$http', '$window'];
+function CodeController($stateParams, $http, $window) {
 
   var self = this;
   self.lesson = $stateParams.lesson;
@@ -14,12 +14,28 @@ function CodeController($stateParams, $http) {
     $http
       .get('https://coffee-coder-api.herokuapp.com/lessons/' + self.lessonId)
       .then(function(res) {
-        self.lesson.title = res.data.lesson.title;
-        self.lesson.summary = res.data.lesson.summary;
-        self.lesson.content = res.data.lesson.content;
-        self.lesson.difficulty = res.data.lesson.difficulty;
-        self.lesson.expectedResult = res.data.lesson.expectedResult;
+        self.lesson = res.data.lesson;
       });
   };
 
+  self.checkResults = function() {
+    // Wait a seond to allow the JS to be evaluated, then check result.
+    setTimeout(function() {
+      var expected = self.lesson.expectedResult;
+      var input = $window.results[0];
+
+      expected == input ? self.match() : self.noMatch();
+      $window.results = [];
+    }, 1000);
+  };
+
+  self.match = function() {
+    console.log('match');
+    $('#code-match').show('fast');
+  };
+
+  self.noMatch = function() {
+    console.log('no match');
+    $('#no-code-match').show('fast');
+  };
 };
